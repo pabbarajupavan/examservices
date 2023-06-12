e40a0e9cf21474d3a7dca3950b6f565d4be40fa5
pabbarajupavan <pavankumar24@live.com>
2023-06-12 11:07:35 +0530

    initial commit

---
+++ src/main/java/com/quest/examservices/ExamservicesApplication.java
@@ -0,0 +1,13 @@
+package com.quest.examservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quest/examservices/ExamservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quest.examservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
