5b653c15181e6411aab46688a49c735689467093
pabbarajupavan <pavankumar24@live.com>
2023-06-21 15:32:39 +0530

    user model created

---
+++ src/main/java/com/quest/examservices/model/User.java
@@ -0,0 +1,25 @@
+package com.quest.examservices.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "user_table")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId ;
+
+    private String userName ;
+    private String password ;
+    private String email ;
+    private String role ;
+    private Long contact ;
+    
+}
