4b58c19d1ee867f7f94c555427a728c31d4f2084
pabbarajupavan <pavankumar24@live.com>
2023-06-23 01:36:44 +0530

    Changed response object to String

--- src/main/java/com/quest/examservices/controller/AdminController.java
+++ src/main/java/com/quest/examservices/controller/AdminController.java
@@ -1,17 +1,15 @@
 package com.quest.examservices.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 import org.springframework.web.bind.annotation.RestController;
 
 import com.quest.examservices.pojo.StudentAccessRequests;
-import com.quest.examservices.pojo.StudentAccessResponse;
+
 import com.quest.examservices.service.StudentService;
 
 @RequestMapping("/api/admin")
@@ -20,12 +18,14 @@ public class AdminController {
 
     @Autowired
     private StudentService studentService ;
+
+    
     
     @RequestMapping(value = "/addStudents", method = RequestMethod.POST,produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody List<StudentAccessResponse> addStudents(@RequestBody StudentAccessRequests studentAccessRequest){
+    public String addStudents(@RequestBody StudentAccessRequests studentAccessRequest){
 
-        List<StudentAccessResponse> responseList = studentService.saveStudent(studentAccessRequest) ;
-        return responseList ;
+        String response = studentService.saveStudent(studentAccessRequest) ;
+        return response ;
     }
 
 }
