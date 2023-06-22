9452a14cd320dd9fe7983ae462ad494d9f5bddc2
pabbarajupavan <pavankumar24@live.com>
2023-06-23 01:37:21 +0530

    changed response to String

--- src/main/java/com/quest/examservices/service/StudentService.java
+++ src/main/java/com/quest/examservices/service/StudentService.java
@@ -1,13 +1,13 @@
 package com.quest.examservices.service;
 
-import java.util.List;
+
 
 
 import com.quest.examservices.pojo.StudentAccessRequests;
-import com.quest.examservices.pojo.StudentAccessResponse;
+
 
 public interface StudentService {
 
-    public List<StudentAccessResponse> saveStudent(StudentAccessRequests studentAccessRequest) ;
+    public String saveStudent(StudentAccessRequests studentAccessRequest) ;
     
 }
