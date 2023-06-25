81f5c3134963d98ba0cbfbc4f82ec2113d1ea9bd
pabbarajupavan <pavankumar24@live.com>
2023-06-25 23:17:49 +0530

    added file upload service

---
+++ src/main/java/com/quest/examservices/service/FileUplaodService.java
@@ -0,0 +1,11 @@
+package com.quest.examservices.service;
+
+import java.io.FileNotFoundException;
+
+
+
+public interface FileUplaodService {
+
+    public String readDataFromFileAndSaveToDB(String filePath,String userName) throws FileNotFoundException ;
+    
+}
