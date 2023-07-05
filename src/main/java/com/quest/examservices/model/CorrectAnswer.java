f0d2ad49aff8b255b83923be015436c43eb87698
pabbarajupavan <pavankumar24@live.com>
2023-07-05 11:44:41 +0530

    commit

---
+++ src/main/java/com/quest/examservices/model/CorrectAnswer.java
@@ -0,0 +1,12 @@
+package com.quest.examservices.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="correct_answer")
+public class CorrectAnswer {
+    
+}
